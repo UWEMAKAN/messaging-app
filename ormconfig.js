085844f16d7db44f56b3c4123169639156ad0606
Uwem Akan <uwemakan12@gmail.com>
2022-07-08 09:37:51 +0100

    added git actions env variables

--- ormconfig.js
+++ ormconfig.js
@@ -4,15 +4,14 @@ const { DataSource } = require('typeorm');
 
 exports.AppDataSource = new DataSource({
   type: 'postgres',
-  port: +process.env.DB_PORT || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'cs_messaging',
-  host: process.env.DB_HOST || 'localhost',
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
   entities: ['src/entities/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
-  migrationsTableName:
-    process.env.DB_MIGRATION_TABLE || 'cs_messaging_migration_table',
+  migrationsTableName: process.env.DB_MIGRATION_TABLE,
   logging: false,
   synchronize: false,
   name: 'appConn',
