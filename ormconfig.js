5b0255e7b930b9b7148f474cb684f52ab7631866
Uwem Akan <uwemakan12@gmail.com>
2022-07-08 09:16:39 +0100

    set default values for datasource props

--- ormconfig.js
+++ ormconfig.js
@@ -4,14 +4,15 @@ const { DataSource } = require('typeorm');
 
 exports.AppDataSource = new DataSource({
   type: 'postgres',
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'cs_messaging',
+  host: process.env.DB_HOST || 'localhost',
   entities: ['src/entities/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
-  migrationsTableName: process.env.DB_MIGRATION_TABLE,
+  migrationsTableName:
+    process.env.DB_MIGRATION_TABLE || 'cs_messaging_migration_table',
   logging: false,
   synchronize: false,
   name: 'appConn',
